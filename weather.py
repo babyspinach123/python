58ceba6b5b26b86ea8c737ed12a3b6da57df1061
babyspinach123 <github@babyspinach.co.uk>
2022-07-16 16:40:43 +0100

    updated weather.py - prints weather in human readable format and adds dictionary to decode weather type

--- weather.py
+++ weather.py
@@ -10,6 +10,42 @@ key = os.getenv('METOFFICE_KEY')
 
 weather_obs_api = 'http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/3354?res=hourly&key='+key
 
+def getWeatherType(type):
+    weatherDict = {
+        "NA" : "Not available",
+        "0" : "Clear night",
+        "1" : "Sunny day",
+        "2" : "Partly cloudy (night)",
+        "3" : "Partly cloudy (day)",
+        "4" : "Not used",
+        "5" : "Mist",
+        "6" : "Fog",
+        "7" : "Cloudy",
+        "8" : "Overcast",
+        "9" : "Light rain shower (night)",
+        "10" : "Light rain shower (day)",
+        "11" : "Drizzle",
+        "12" : "Light rain",
+        "13" : "Heavy rain shower (night)",
+        "14" : "Heavy rain shower (day)",
+        "15" : "Heavy rain",
+        "16" : "Sleet shower (night)",
+        "17" : "Sleet shower (day)",
+        "18" : "Sleet",
+        "19" : "Hail shower (night)",
+        "20" : "Hail shower (day)",
+        "21" : "Hail",
+        "22" : "Light snow shower (night)",
+        "23" : "Light snow shower (day)",
+        "24" : "Light snow",
+        "25" : "Heavy snow shower (night)",
+        "26" : "Heavy snow shower (day)",
+        "27" : "Heavy snow",
+        "28" : "Thunder shower (night)",
+        "29" : "Thunder shower (day)",
+        "30" : "Thunder"}
+    return(weatherDict[type])
+
 response = requests.get(weather_obs_api)
 results = response.json()
 print('\n----- response ------\n')
@@ -42,7 +78,15 @@ for i in period:
     if i['value'] == current_date:
         # print(i['Rep'])
         for ob in i['Rep']:
-            if int(ob['$']) == int(current_hour) * 60:
+            if int(ob['$']) == (int(current_hour)-1) * 60: # Need to -1 hour as latest observation is from previous hour
                 print(ob)
                 print(f'Temprature: {ob["T"]} degC')
-    
+                print(f'Visability: {ob["V"]} m')
+                print(f'Wind Direction: {ob["D"]}')
+                print(f'Wind Speed: {ob["S"]} mph')
+                print(f'Wind Gust: {ob["G"]} mph')
+                print(f'Weather Type: {getWeatherType(ob["W"])}')
+                print(f'Pressure: {ob["P"]} hpa (mBar)')
+                print(f'Pressure Trend: {ob["Pt"]}')
+                print(f'Dew Point: {ob["Dp"]} degC')
+                print(f'Relative Humidity: {ob["H"]} %')
